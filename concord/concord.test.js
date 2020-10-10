520860d42caa258f5ac37241ccd226e3f7cbf7ff
JP Kramer <jpkramer707_github@mm.st>
2020-10-10 05:27:08 +0000

    Cleanup

--- concord/concord.test.js
+++
@@ -1,5 +0,0 @@
-const { concord } = require('./concord');
-const { client } = require("../craig/client.js");
-jest.mock("../craig/client.js");
-client.editGuildMember.mockResolvedValue({});
-
--- concord/eventEmitter.js
+++ concord/eventEmitter.js
@@ -1,7 +1,6 @@
 const EventEmitter = require('eventemitter3');
 const { LOG } = require('./constants').EVENTS;
 const { argsToArray } = require('./util');
-const eventEmitter = new EventEmitter();
 
 const log = function() { eventemitter3.emit(LOG, argsToArray(arguments, 0)); };
 
